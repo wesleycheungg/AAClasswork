2d0222e3c160732bc899ef74f13f3487d24d6780
MalachiCoberley <malachicoberley@gmail.com>
2021-09-22 17:31:00 -0700

    Finalize most movements

--- W4/D2/chess/rook.rb
+++ W4/D2/chess/rook.rb
@@ -12,22 +12,13 @@ class Rook < Piece
 
   def valid_moves
     all_moves(self.curr_pos).each do |el|
-      
-    end
-  end
 
-  def all_moves(current_pos)
-    moves = []
-    row, col = current_pos
-    (0..7).each do |n|
-      moves << [row, n]
-      moves << [n, col]
     end
-    moves
   end
 
   private
   def moves_dirs
+    horizontal_dirs
   end
 
 end
--- W4/D2/chess/slideable.rb
+++ W4/D2/chess/slideable.rb
@@ -2,9 +2,46 @@ module Slideable
 
     # private
   
-  HORIZONTAL_DIRS = []
-  DIAGONAL_DIRS = []
+  HORIZONTAL_DIRS = [
+    [0,1],
+    [0,-1],
+    [1,0],
+    [-1,0]
+  ]
+  DIAGONAL_DIRS = [
+    [1,1],
+    [1,-1],
+    [-1,1],
+    [-1,-1]
+]
 
+  def horizontal_dirs
+    HORIZONTAL_DIRS
+  end
+
+
+  def moves
+    # calls moves_dir
+    valid_moves = []
+    moves_dir.each do |el|
+      valid_moves.concat(grow_unblocked_moves_in_dir(el))
+    end
+  end
+
+  def grow_unblocked_moves_in_dir(arr)
+    result =[]
+    enemy = false
+    while enemy == false
+      if arr.any? {|ele| ele > 7 || ele < 0}
+        return result
+      elsif board[arr] != nil && board[arr].color == self.color
+        return result
+      elsif board[arr] != nil && board[arr].color != self.color
+        enemy = true
+      end
+    end
+    
+  end
 
   # def left_moves(curr_pos)
   #   row, col = curr_pos
