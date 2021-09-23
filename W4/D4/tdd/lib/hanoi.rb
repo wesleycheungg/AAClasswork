5e454ad5f2054e08ffc02f888c547e4893436c09
kw-8 <kwu11235813@gmail.com>
2021-09-23 16:33:53 -0700

    hanoi moves partly done

--- W4/D4/tdd/lib/hanoi.rb
+++ W4/D4/tdd/lib/hanoi.rb
@@ -2,6 +2,15 @@ class Hanoi
   attr_reader :stacks
   def initialize
     @stacks = Array.new(3) {[]}
-    (1..3).each {|ele| @stacks[0] << ele}
+    (1..3).each {|ele| @stacks[0].unshift(ele)}
+  end
+
+  def move(first_stack, second_stack)
+    disk = @stacks[first_stack].pop
+    @stacks[second_stack].push(disk)
+  end
+
+  def won?
+    @stacks[-1].length == 3
   end
 end
--- W4/D4/tdd/spec/hanoi_spec.rb
+++ W4/D4/tdd/spec/hanoi_spec.rb
@@ -8,13 +8,30 @@ describe Hanoi do
     it "has 3 disks" do
       expect(h.stacks.flatten.length).to eq(3)
     end
+    it "disks have unique sizes" do
+      expect(h.stacks.flatten).to eq(h.stacks.flatten.uniq)
+    end
+  end
+
+  describe "#move" do
+    it "raise error if inputs are not valid" do
+      expect{move(0,4)}.to raise_error
+    end
+
+    it "successfully moves disk from first to second stack" do
+      expect(move(0,1)).to eq(true)
+    end
+
+    it "does not move a bigger disk on top of a smaller disk" do
+      expect{move(0,2)}.to raise_error
+    end
   end
 
   describe "#won?" do
     it "all disks in last stack" do 
       expect(h.stacks[-1].length).to eq(true)
     end
-    it "not all disks in last stack"
+    it "not all disks in last stack" do
       expect(h.stacks[-1].length).to eq(false)
     end
 
