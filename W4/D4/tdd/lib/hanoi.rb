a9109ed3f43270e54efc0bb44d7ee92d237ba990
wesley <cheung.w.2016@gmail.com>
2021-09-23 17:30:04 -0700

    debug move method

--- W4/D4/tdd/lib/hanoi.rb
+++ W4/D4/tdd/lib/hanoi.rb
@@ -1,3 +1,5 @@
+require ("byebug")
+
 class Hanoi
   attr_reader :stacks
   def initialize
@@ -6,8 +8,12 @@ class Hanoi
   end
 
   def move(first_stack, second_stack)
+    raise "Inputs invalid!" unless [0,1,2].include?(first_stack) && [0,1,2].include?(second_stack)
+    # debugger
+    raise "Can't put bigger disk on top of smaller disk." if !@stacks[second_stack].empty? && @stacks[second_stack][-1] < @stacks[first_stack][-1]
     disk = @stacks[first_stack].pop
     @stacks[second_stack].push(disk)
+    return true
   end
 
   def won?
--- W4/D4/tdd/spec/hanoi_spec.rb
+++ W4/D4/tdd/spec/hanoi_spec.rb
@@ -1,5 +1,7 @@
 require "hanoi"
 describe Hanoi do
+  subject(:hanoi) {Hanoi.new}
+
   describe "#initialize" do
     h = Hanoi.new
     it "call stacks" do
@@ -14,25 +16,29 @@ describe Hanoi do
   end
 
   describe "#move" do
+    
     it "raise error if inputs are not valid" do
-      expect{move(0,4)}.to raise_error
+      expect{hanoi.move(0,4)}.to raise_error ("Inputs invalid!")
     end
 
     it "successfully moves disk from first to second stack" do
-      expect(move(0,1)).to eq(true)
+      expect(hanoi.move(0,1)).to eq(true)
     end
 
     it "does not move a bigger disk on top of a smaller disk" do
-      expect{move(0,2)}.to raise_error
+      expect{hanoi.move(0,1)}.to raise_error ("Can't put bigger disk on top of smaller disk.")
     end
   end
 
   describe "#won?" do
+
     it "all disks in last stack" do 
-      expect(h.stacks[-1].length).to eq(true)
+      allow(:hanoi).to receive (stacks).and_return([[],[], [3,2,1]])
+      expect(hanoi.stacks[-1].length).to eq(true)
     end
     it "not all disks in last stack" do
-      expect(h.stacks[-1].length).to eq(false)
+      allow(:hanoi).to receive (stacks).and_return([[],[], [3,2,1]])
+      expect(hanoi.stacks[-1].length).to eq(false)
     end
 
   end
