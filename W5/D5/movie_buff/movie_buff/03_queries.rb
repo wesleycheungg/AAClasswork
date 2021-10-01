56d62b2fe4865c9bb6a32e5cef21a05f09c47090
Williamsliam23 <williams.liam23@gmail.com>
2021-10-01 17:47:55 -0500

    03 start

--- W5/D5/movie_buff/movie_buff/03_queries.rb
+++ W5/D5/movie_buff/movie_buff/03_queries.rb
@@ -1,7 +1,15 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  # Movie
+  #   .select(:title, :id)
+  #   .joins(:actors)
+  #   .where(actors: {name: those_actors}, castings: {ord: 1})
+  Actor
+    .select('movies.id, movies.title')
+    .joins(:movies)
+    .where('actors.name in (?)', those_actors)
+    
 end
 
 def golden_age
