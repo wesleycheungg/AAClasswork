7895fb98965c798884958b188f640cb12dff2443
wesley <cheung.w.2016@gmail.com>
2021-10-26 13:58:00 -0700

    finished setupBoard method

--- W9/D2/tic_tac_toe/src/index.js
+++ W9/D2/tic_tac_toe/src/index.js
@@ -1,6 +1,19 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");
+
+const Game = require("./ttt_node/game.js");
+
+new Game();
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const tView = document.querySelector(".ttt");
+
+
 });
+
+View.prototype.setupBoard = function(){
+  const grid = document.querySelector(".grid");
+  grid.innerHTML = (new Array(9)).map( (item) => {
+    return `<li>${item}</li>`
+  })
+}
