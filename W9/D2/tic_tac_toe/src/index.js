be5e25b2a82184c8567d413c68db9998c7b3e208
tharpercp <tharpercp@gmail.com>
2021-10-26 14:27:39 -0700

    working on board render

--- W9/D2/tic_tac_toe/src/index.js
+++ W9/D2/tic_tac_toe/src/index.js
@@ -7,13 +7,24 @@ new Game();
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   const tView = document.querySelector(".ttt");
-
+  this.setupBoard();
 
 });
 
 View.prototype.setupBoard = function(){
-  const grid = document.querySelector(".grid");
+  const grid = document.querySelector(".make-grid");
   grid.innerHTML = (new Array(9)).map( (item) => {
-    return `<li>${item}</li>`
+    return `  <ul class="grid">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                // <li></li>    const li = document.createElement("li")
+                                *dont forget to append UL to something (gameview?)
+              </ul>`
   })
 }
