87b36f8bff8231f94b4e893949e94b9d97b86122
wesley <cheung.w.2016@gmail.com>
2021-10-26 15:32:14 -0700

    got the view to work

--- W9/D2/tic_tac_toe/src/index.js
+++ W9/D2/tic_tac_toe/src/index.js
@@ -1,30 +1,14 @@
 const View = require("./ttt-view.js");
 
-const Game = require("./ttt_node/game.js");
+const Game = require("../ttt_node/game.js");
 
-new Game();
+let newGame = new Game();
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   const tView = document.querySelector(".ttt");
-  this.setupBoard();
-
+  let view = new View(newGame, tView)
 });
 
-View.prototype.setupBoard = function(){
-  const grid = document.querySelector(".make-grid");
-  grid.innerHTML = (new Array(9)).map( (item) => {
-    return `  <ul class="grid">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                // <li></li>    const li = document.createElement("li")
-                                *dont forget to append UL to something (gameview?)
-              </ul>`
-  })
-}
+
+
--- W9/D2/tic_tac_toe/src/ttt-view.js
+++ W9/D2/tic_tac_toe/src/ttt-view.js
@@ -1,7 +1,24 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.el = el
+    this.game = game
+    
+    this.setupBoard()
 
-  setupBoard() {}
+  }
+
+  setupBoard() {
+    // const grid = document.querySelector(".make-grid");
+    // debugger
+    const grid_ul = document.createElement("ul");
+    this.el.append(grid_ul);
+    // const liElement = document.createElement("li").append(grid_ul);
+    for (let i = 0; i < 3; i++) {
+      grid_ul.append(document.createElement("li"));
+      grid_ul.append(document.createElement("li")); //.className = `${i}2`
+      grid_ul.append(document.createElement("li")); //.className = `${i}3`
+    }
+  }
   
   bindEvents() {}
 
