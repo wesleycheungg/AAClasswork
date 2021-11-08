15f1271dff48021af8f9f4b51fcec17066e89f32
wesley <cheung.w.2016@gmail.com>
2021-11-08 11:59:28 -0800

    finished Tile display method

--- W11/D1/minesweeper/components/tile.jsx
+++ W11/D1/minesweeper/components/tile.jsx
@@ -4,11 +4,25 @@ import * as Minesweeper from "../minesweeper";
 class Tile extends React.Component {
   constructor(props) {
     super(props);
+
+    this.displayVal = "T"
+  }
+
+  getDisplay(){
+    let tile = this.props.cell
+    if(tile.explored){
+      this.displayVal = (tile.adjacentBombCount() > 0 ? tile.adjacentBombCount() : "E")
+    } else if (tile.flagged) {
+      this.displayVal = "F"
+    } else if (tile.bombed) {
+      this.displayVal = "B"
+    }
+    return this.displayVal
   }
 
   render() {
     return (
-      "T"
+      <div>{this.getDisplay()}</div>
     )
   }
 }
