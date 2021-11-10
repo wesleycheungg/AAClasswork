4b091b5cd2e97ba6c3442ee11b264e32e0128757
wesley <cheung.w.2016@gmail.com>
2021-11-10 13:46:29 -0800

    added App component

---
+++ W11/D2/todos/frontend/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => (
+    <h1>This is working so far...</h1>
+);
+
+export default App;
--- W11/D2/todos/frontend/todo_redux.jsx
+++ W11/D2/todos/frontend/todo_redux.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom"
 import configureStore from "./store/store"
 import Util from "./actions/todo_actions"
 import { receiveSteps, receiveStep, removeStep} from "./actions/step_actions"
+import App from "./components/app"
 
 document.addEventListener("DOMContentLoaded", () => {
     const content = document.getElementById("content");
-    ReactDOM.render(<h1>Todos App</h1>, content);
+    ReactDOM.render(<App />, content);
     const store = configureStore();
     window.store = store;
     window.receiveTodos = Util.receiveTodos;
