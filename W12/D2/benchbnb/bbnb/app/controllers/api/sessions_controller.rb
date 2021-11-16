0f6088d9c0146554597b8fc81830a0f818cbc44e
wesley <cheung.w.2016@gmail.com>
2021-11-16 14:59:51 -0800

    finished controllers

--- W12/D2/benchbnb/bbnb/app/controllers/api/sessions_controller.rb
+++ W12/D2/benchbnb/bbnb/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,12 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user
+            login(@user)
+        end
+    end
+
+    def destroy
+        logout!
+    end
 end
--- W12/D2/benchbnb/bbnb/app/controllers/api/users_controller.rb
+++ W12/D2/benchbnb/bbnb/app/controllers/api/users_controller.rb
@@ -1,2 +1,13 @@
 class Api::UsersController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
