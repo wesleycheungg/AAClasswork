971ac5d819d76d4ffe1f8688146a1360ff1b2149
wesley <cheung.w.2016@gmail.com>
2021-12-05 17:40:30 -0800

    first commit, finished backend

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+        extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello a/A")
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save()
+    res.send("Hello a/A");
 });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/api/users.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+const passport = require('passport');
+
+router.get("/test", (req, res) => {
+    res.json({ msg: "This is the user route"})
+});
+
+
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({msg: 'Success'});
+})
+
+router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ handle: req.body.handle }).then(user => {
+        if (user) {
+            errors.handle = "User already exists";
+            return res.status(400).json(errors);
+        } else {
+            const newUser = new User({
+                handle: req.body.handle,
+                email: req.body.email,
+                password: req.body.password
+            })
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then((user) => {
+                            const payload = { id: user.id, handle: user.handle };
+                            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            });
+                        })
+                        .catch(err => console.log(err))
+                });
+            });
+            // newUser.save().then( user => res.send(user)).catch(err => res.send(err));
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    const handle = req.body.handle;
+    const password = req.body.password
+
+    User.findOne({ handle })
+        .then(user => {
+            if(!user) {
+                errors.handle = "This user does not exist";
+                return res.status(400).json(errors);
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        const payload = {
+                            id: user.id,
+                            handle: user.handle,
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            }
+                        )
+                    } else {
+                        errors.password = "Incorrect password";
+                        return res.status(400).json(errors);
+                    }
+                })
+        })
+})
+
+
+module.exports = router;
